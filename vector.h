36d3c28b7d9f9fd0525b42834ae97fab782f9417
hsz <sdfghjk>
2022-11-06 21:25:06 +0100

    header file update n some thoughts on the usefullness of these boundary checks

--- vector.h
+++ vector.h
@@ -1,10 +1,6 @@
 // Checks if _CUSTOM_VECTOR_ IF DECLARED
 #ifndef _CUSTOM_VECTOR_
 
-// info for this bs commit:
-//  what this exercise wants is probably way simpler than im thinking, doesnt rlly make sense as it is no real improvement and only covers some cases
-//  otherwise the predefined constructor prototypes would need to be changed
-
 // Defines _CUSTOM_VECTOR_ if above
 // conditions fails
 #define _CUSTOM_VECTOR_
@@ -82,13 +78,50 @@ public:
 
     private:
         pointer ptr; // Points to an element in Vector.
+
+        // boundary and * checks
+        //  as far as my understanding goes it makes sense for iterators to NOT implement these checks but move them outside of the cls n handle in the context of the container
+        //  also it doesnt rlly add any guarantees as there is a constructor with a pointer which has no way of checking anything.
+
+        // thought:
+        //  keep track of end, only use case would be a begin ptr that is incremented
+        //  would be more easy to store a ptr to the vec and on increment, deref get the end ptr
+
+        // cases:
+        // default constructor
+        //      no deref or inc
+        //      only valid if assignment happens. assign:
+        //          iterator w ref
+        //              deref and inc: safety
+        //          iterator with ptr constructor
+        //              deref and inc: no safety (have to allow for it to work and have faith)
+        // ptr constructor
+        //      always deref and inc (on faith) - this is what makes no sense but its in the requirement
+        // ...
+
+        // whats the usefullness of these checks?
+        // are there actually any real world cases where this would provide incresed safety?
+        // was thinking in a wrong directon.. figuring out how to make the iterator never get invalidated.
+        // but thats not what they for or how they used..
+        // if u have a for loop rlly all u can do is loop from begin() to end()
+        // only thing it could prevent is
+        // <= instead of <
+        // if u then push and loop again u need new iterators and the old ones r invalidated and thats how its supposed to be.
+        //  thought im thinking to much and got hung up on shit they didnt even intend but they want this!!!
+        //  > Für die grundsätzliche Absicherung reicht das Bestehen der Testcases ohne "Strongly" im Namen.
+        //  > Für die Testcases mit "Strongly" im Namen muss man eine Verbindung zwischen dem Iterator und dem zugehörigen Vector-Objekt einrichten,
+        //  > so dass Änderungen im Vektor auch im Iterator reflektiert werden.
+
+        // todo: research nested cls in cpp, is there any point in putting iter cls inside? other thatn hinting at its relation (with which its intended to be used with)
+
         bool incrementable;
         bool dereferencable;
-        Vector &vec;
+        Vector *vec;
 
     public:
-        Iterator();                       // Returns an iterator on nullptr.
-        Iterator(pointer ptr, Vector &v); // Returns an iterator which sets the instance variable to ptr.
+        Iterator();                                                                        // Returns an iterator on nullptr.
+        Iterator(pointer ptr);                                                             // Returns an iterator which sets the instance variable to ptr.
+        Iterator(pointer ptr, bool incrementable, bool dereferencable, const Vector *vec); // Returns a "safe" iterator with boundary and deref checks.
 
         // todo: check if const is necessary
         reference operator*() const;                     // Returns the value of the value referenced by ptr.
@@ -111,12 +144,16 @@ public:
 
     private:
         pointer ptr; // Points to an element in Vector.
-        const Vector &vec;
+        bool incrementable;
+        bool dereferencable;
+        Vector *vec;
 
     public:
-        ConstIterator();                             // Returns a ConstIterator on nullptr.
-        ConstIterator(pointer ptr, const Vector &v); // Returns a ConstIterator which sets the instance variable to ptr.
+        ConstIterator();                                                                        // Returns a ConstIterator on nullptr.
+        ConstIterator(pointer ptr);                                                             // Returns a ConstIterator which sets the instance variable to ptr.
+        ConstIterator(pointer ptr, bool incrementable, bool dereferencable, const Vector *vec); // Returns a "safe" iterator with boundary and deref checks.
 
+        pointer get_ptr_unsafe() const;
         reference operator*() const;                   // Returns the value of the value referenced by ptr.
         pointer operator->() const;                    // Returns a pointer to the referenced value.
         bool operator==(const const_iterator &) const; // Compares the pointers for equality. (A global function may be a better choice).
