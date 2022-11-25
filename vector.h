314c22d88ea6e1c9be23538c9f7ede9d12127c7c
hsz <sdfghjk>
2022-11-25 16:57:08 +0100

    fuer test

--- vector.h
+++ vector.h
@@ -1,3 +1,77 @@
+
+// PROTOTYPES
+// class Vector
+// {
+// public:
+//     Vector();                                  // default
+//     Vector(const Vector &v);                   // copy
+//     Vector(size_t n);                          // Returns a Vector with space for n elements.
+//     Vector(std::initializer_list<value_type>); // Returns a Vector with specified content.
+//     ~Vector();                                 // destructor
+
+//     // Copy assignment operator: The this object takes the values from the parameter. (Necessary
+//     const Vector &operator=(const Vector &other);
+
+//     size_t size() const;                          // Returns number of saved elements.
+//     bool empty() const;                           // Returns true if the Vector is empty, otherwise false.
+//     void clear();                                 // Deletes all elements from Vector.
+//     void reserve(size_t n);                       // Capacity of the Vector is increased to n if it is not already at least this large.
+//     void shrink_to_fit();                         // Capacity is reduced to number of elements.
+//     void push_back(value_type x);                 // Adds a copy of x to the end of the Vector.
+//     void pop_back();                              // Removes the last element in the Vector. Throws an std::runtime_error exception if the Vector was empty.
+//     double &operator[](size_t index);             // Returns the element at the given position (index). If index is out of bounds, throws an std::runtime_error exception
+//     const double &operator[](size_t index) const; // Returns the element at the given position (index). If index is out of bounds, throws an std::runtime_error exception
+//     size_t capacity() const;                      // Returns current capacity of the Vector.
+//     ostream &print(ostream &o) const;
+
+//     // iterator
+//     iterator begin();
+//     iterator end();
+//     const_iterator begin() const;
+//     const_iterator end() const;
+
+//     iterator erase(const_iterator pos);
+//     iterator insert(const_iterator pos, const_reference val);
+
+//     class Iterator
+//     {
+//     public:
+//         Iterator();                               // Returns an iterator on nullptr.
+//         Iterator(pointer ptr);                    // Returns an iterator which sets the instance variable to ptr.
+//         Iterator(pointer ptr, const Vector *vec); // Returns a "safe" iterator with boundary and deref checks.
+
+//         pointer get_ptr_unsafe() const;
+//         reference operator*() const;                     // Returns the value of the value referenced by ptr.
+//         pointer operator->() const;                      // Returns a pointer to the referenced value.
+//         bool operator==(const const_iterator &it) const; // Compares the pointers for equality. (A global function may be a better choice).
+//         bool operator!=(const const_iterator &it) const; // Compares the pointers for inequality. (A global function may be a better choice).
+//         iterator &operator++();                          // (Prefix) Iterator points to next element and (a reference to it) is returned.
+//         iterator operator++(int);                        // (Postfix) Iterator points to next element. Copy of iterator before increment is returned.
+//         operator const_iterator() const;                 // (Type conversion) Allows to convert Iterator to ConstIterator
+//     };
+
+//     class ConstIterator
+//     {
+//     public:
+//         ConstIterator();                               // Returns a ConstIterator on nullptr.
+//         ConstIterator(pointer ptr);                    // Returns a ConstIterator which sets the instance variable to ptr.
+//         ConstIterator(pointer ptr, const Vector *vec); // Returns a "safe" iterator with boundary and deref checks.
+
+//         pointer get_ptr_unsafe() const;
+//         reference operator*() const;                   // Returns the value of the value referenced by ptr.
+//         pointer operator->() const;                    // Returns a pointer to the referenced value.
+//         bool operator==(const const_iterator &) const; // Compares the pointers for equality. (A global function may be a better choice).
+//         bool operator!=(const const_iterator &) const; // Compares the pointers for inequality. (A global function may be a better choice).
+//         const_iterator &operator++();                  // (Prefix) Iterator points to next element and (a reference to it) is returned.
+//         const_iterator operator++(int);                // (Postfix) Iterator points to next element. Copy of iterator before increment is returned.
+
+//         friend Vector::difference_type operator-(const Vector::ConstIterator &lop,
+//                                                  const Vector::ConstIterator &rop);
+//     };
+// };
+
+// ostream &operator<<(ostream &o, const Vector &v);
+
 // Checks if _CUSTOM_VECTOR_ IF DECLARED
 #ifndef _CUSTOM_VECTOR_
 
@@ -8,14 +82,14 @@
 #include <iostream>
 using namespace std;
 
-template <typename value_type>
+template <typename T>
 class Vector
 {
     // iterator stuff
 public:
     class ConstIterator;
     class Iterator;
-    // using value_type = double;
+    using value_type = T;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
     using reference = value_type &;
@@ -66,21 +140,8 @@ public:
         {
             values[i++] = elem;
         }
-
-        // can i directly use the initializer list ptr? or do i need to re-allocate??
-        // according to doc, initializer list is stored as an array
-        // did some research:
-        //          https://stackoverflow.com/questions/8193102/initializer-list-and-move-semantics
-        //          https://tristanbrindle.com/posts/beware-copies-initializer-list
-        //      apparently initializer list only provides const access and the elements cant be moved
-        //      its all copied in the std vector.
-        //      dont rlly understand y its designed that way...
-        // might try constructor to pass an array and take ownership
-        // conclusion:
-        //      initializer lists r inefficient
     }
 
-    // todo: test valgrind and test special cases like Vector(0) and Vector({})
     ~Vector()
     {
         delete[] values;
@@ -213,9 +274,6 @@ public:
 
     const_iterator begin() const
     {
-        // todo: y does that count as Iterator
-        // return values;
-
         return const_iterator(values, this);
     }
 
@@ -254,7 +312,7 @@ public:
     class Iterator
     {
     public:
-        // using value_type = Vector::value_type;
+        using value_type = Vector::value_type;
         using reference = Vector::reference;
         using pointer = Vector::pointer;
         using difference_type = Vector::difference_type;
@@ -263,44 +321,9 @@ public:
     private:
         pointer ptr; // Points to an element in Vector.
 
-        // boundary and * checks
-        //  as far as my understanding goes it makes sense for iterators to NOT implement these checks but move them outside of the cls n handle in the context of the container
-        //  also it doesnt rlly add any guarantees as there is a constructor with a pointer which has no way of checking anything.
-
-        // thought:
-        //  keep track of end, only use case would be a begin ptr that is incremented
-        //  would be more easy to store a ptr to the vec and on increment, deref get the end ptr
-
-        // cases:
-        // default constructor
-        //      no deref or inc
-        //      only valid if assignment happens. assign:
-        //          iterator w ref
-        //              deref and inc: safety
-        //          iterator with ptr constructor
-        //              deref and inc: no safety (have to allow for it to work and have faith)
-        // ptr constructor
-        //      always deref and inc (on faith) - this is what makes no sense but its in the requirement
-        // ...
-
-        // whats the usefullness of these checks?
-        // are there actually any real world cases where this would provide incresed safety?
-        // was thinking in a wrong directon.. figuring out how to make the iterator never get invalidated.
-        // but thats not what they for or how they used..
-        // if u have a for loop rlly all u can do is loop from begin() to end()
-        // only thing it could prevent is
-        // <= instead of <
-        // if u then push and loop again u need new iterators and the old ones r invalidated and thats how its supposed to be.
-        //  thought im thinking to much and got hung up on shit they didnt even intend but they want this!!!
-        //  > Für die grundsätzliche Absicherung reicht das Bestehen der Testcases ohne "Strongly" im Namen.
-        //  > Für die Testcases mit "Strongly" im Namen muss man eine Verbindung zwischen dem Iterator und dem zugehörigen Vector-Objekt einrichten,
-        //  > so dass Änderungen im Vektor auch im Iterator reflektiert werden.
-
-        // todo: research nested cls in cpp, is there any point in putting iter cls inside? other thatn hinting at its relation (with which its intended to be used with)
-
         const Vector *vec;
 
-        bool checkIncrementable() const
+        bool check_deref_inc() const
         {
             return (vec != nullptr) && (ptr != vec->end().get_ptr_unsafe());
         }
@@ -315,7 +338,6 @@ public:
                                 vec{nullptr} {}
 
         // Returns a "safe" iterator with boundary and deref checks.
-        // todo: y const vector
         Iterator(pointer ptr, const Vector *vec) : ptr{ptr},
                                                    vec{vec} {}
 
@@ -325,19 +347,16 @@ public:
             return ptr;
         }
 
-        // todo: check if const is necessary
         reference operator*() const // Returns the value of the value referenced by ptr.
         {
-            // if (!dereferencable)
-            //     throw runtime_error("iterator is not dereferencable");
-            if (!checkIncrementable())
+            if (!check_deref_inc())
                 throw runtime_error("end is not dereferencable");
             return *ptr;
         }
 
         pointer operator->() const // Returns a pointer to the referenced value.
         {
-            if (!checkIncrementable())
+            if (!check_deref_inc())
                 throw runtime_error("iterator is not dereferencable");
             return ptr;
         }
@@ -354,7 +373,7 @@ public:
 
         iterator &operator++() // (Prefix) Iterator points to next element and (a reference to it) is returned.
         {
-            if (checkIncrementable())
+            if (check_deref_inc())
             {
                 ++ptr;
             }
@@ -367,7 +386,7 @@ public:
 
         iterator operator++(int) // (Postfix) Iterator points to next element. Copy of iterator before increment is returned.
         {
-            iterator pre = checkIncrementable() ? iterator(ptr++, vec) : *this;
+            iterator pre = check_deref_inc() ? iterator(ptr++, vec) : *this;
             return pre;
         }
 
@@ -380,7 +399,7 @@ public:
     class ConstIterator
     {
     public:
-        // using value_type = Vector::value_type;
+        using value_type = Vector::value_type;
         using reference = Vector::const_reference;
         using pointer = Vector::const_pointer;
         using difference_type = Vector::difference_type;
@@ -389,7 +408,7 @@ public:
     private:
         pointer ptr; // Points to an element in Vector.
         const Vector *vec;
-        bool checkIncrementable() const
+        bool check_deref_inc() const
         {
             return vec != nullptr && ptr != vec->end().get_ptr_unsafe();
         }
@@ -413,14 +432,14 @@ public:
 
         reference operator*() const // Returns the value of the value referenced by ptr.
         {
-            if (!checkIncrementable())
+            if (!check_deref_inc())
                 throw runtime_error("constiterator is not dereferencable**");
             return *ptr;
         }
 
         pointer operator->() const // Returns a pointer to the referenced value.
         {
-            if (!checkIncrementable())
+            if (!check_deref_inc())
                 throw runtime_error("constiterator is not dereferencable--");
             return &*ptr;
         }
@@ -437,7 +456,7 @@ public:
 
         const_iterator &operator++() // (Prefix) Iterator points to next element and (a reference to it) is returned.
         {
-            if (checkIncrementable())
+            if (check_deref_inc())
             {
                 ++ptr;
             }
@@ -446,7 +465,7 @@ public:
 
         const_iterator operator++(int) // (Postfix) Iterator points to next element. Copy of iterator before increment is returned.
         {
-            const_iterator pre = checkIncrementable() ? const_iterator(ptr++, vec) : *this;
+            const_iterator pre = check_deref_inc() ? const_iterator(ptr++, vec) : *this;
             return pre;
         }
 
